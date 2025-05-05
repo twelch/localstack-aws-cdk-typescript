f2a7f5ab932b7cd53b919455137a3bed24e1bb2e
Tim Welch <tim.j.welch@gmail.com>
2025-05-04 18:36:38 -0700

    override endpoint url to localhost IP

--- start.js
+++ start.js
@@ -8,12 +8,12 @@ const run = () => {
   shell.env["AWS_REGION"] = "us-east-1"
 
   shell.exec(`localstack stop`)
-  shell.exec(`localstack start -d --no-banner`)
+  shell.exec(`AWS_ENDPOINT_URL=http://127.0.0.1:4566 AWS_ENDPOINT_URL_S3=http://127.0.0.1:4566 localstack start -d --no-banner`)
   shell.exec(`localstack config show`)
 
   shell.cd("cdk")
   shell.exec(`cdklocal bootstrap`)
-  shell.exec(`cdklocal deploy --require-approval never`)
+  shell.exec(`AWS_ENDPOINT_URL=http://127.0.0.1:4566 AWS_ENDPOINT_URL_S3=http://127.0.0.1:4566 cdklocal deploy --require-approval never`)
 
   shell.exec(
     `awslocal apigateway get-rest-apis --output json --query "items[0].{apiId: id}" > ../frontend/src/.local.json`
